760a27b38abd612b6eb8fcf4a232423c4c23b231
Gervásio Neto <gervasiolopesn@gmail.com>
2024-10-22 19:18:10 -0300

    Funções de operação no app.js

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ app.listen(port, ()=> {
     console.log(`Servidor express rodando na porta ${port}`)
 })
 
+function soma(a, b) {
+  return a + b;
+}
+function subtracao(a, b) {
+  return a - b;
+}
+function divisao(a, b) {
+  return a/b;
+}
+function multiplicacao(a, b) {
+  return a * b;
+}
