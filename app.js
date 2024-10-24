e095eca2b30a2b80e9887a0d375b1a93a469dd1c
Gervasio Neto <gervasiolopesn@gmail.com>
2024-10-24 15:26:44 -0300

    Adicionado resquisições POST das operações

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
 const express = require("express"); 
 const app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const port = 3000;
 
+// Inícial
+app.get("/", function (req, res) {
+  res.send(`
+      <html>
+          <head>
+              <title>Bem-vindo</title>
+          </head>
+          <body>
+              <h1>Bem-vindo ao meu site!</h1>
+              <p>Esta é a página inicial.</p>
+          </body>
+      </html>
+  `);
+});
+
+
+
 app.listen(port, ()=> {
-    console.log(`Servidor express rodando na porta ${port}`)
+    console.log(`Servidor express rodando na porta http://localhost:${port}/`)
 })
+
+app.post('/soma', function (req, res) {
+    var body = req.body;
+    var resultado = soma(body.a, body.b);
+    
+    res.send(`O resultado da soma de ${body.a} + ${body.b} é ${resultado}`);
+  });
+
+app.post('/subtracao', function (req, res) {
+    var body = req.body;
+    var resultado = subtracao(body.a, body.b);
+    
+    res.send(`O resultado da Subtração de ${body.a} - ${body.b} é ${resultado}`);
+});
+
+app.post('/divisao', function (req, res) {
+    var body = req.body;
+    var resultado = divisao(body.a, body.b);
+    
+    res.send(`O resultado da Divisão de ${body.a} / ${body.b} é ${resultado}`);
+});
+  
+app.post('/multiplicacao', function (req, res) {
+    var body = req.body;
+    var resultado = multiplicacao(body.a, body.b);
+    
+    res.send(`O resultado da Multiplicação de ${body.a} * ${body.b} é ${resultado}`);
+});
 
 function soma(a, b) {
   return a + b;
